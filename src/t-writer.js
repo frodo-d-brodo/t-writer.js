8746510ebdb0874614723b7437208951389d03a8
jp <multimediamediaventures@gmail.com>
2023-10-29 15:23:29 -0600

    fix prepend logic

--- src/t-writer.js
+++ src/t-writer.js
@@ -330,7 +330,7 @@ class Typewriter {
 
         // -> If limit would be surpassed while printing this word, return newline
         const mustPrependNewLine = arrayRange(currentWordTrueBounds.startIndex, currentWordTrueBounds.endIndex, 1)
-          .some(x => x % (this.options.wordWrapLineLengthLimit) === 0);
+          .some(x => x % (this.options.wordWrapLineLengthLimit) === 1);
 
         if (mustPrependNewLine) {
           this.extraNewlineCount++;
